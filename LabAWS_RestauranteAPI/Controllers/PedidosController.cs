bc3d825eb9c429d357beffc0d7916c6552c51bb0
karinaareco <165727965+karinaareco@users.noreply.github.com>
2024-08-21 20:15:25 -0300

    Correccion endpoint Post (Kari)

--- LabAWS_RestauranteAPI/Controllers/PedidosController.cs
+++ LabAWS_RestauranteAPI/Controllers/PedidosController.cs
@@ -12,14 +12,14 @@ namespace LabAWS_RestauranteAPI.Controllers
     {
 
         private readonly RestauranteContext _context;
+        private static List<Pedido> _pedidos = new List<Pedido>();
+
         public PedidosController(RestauranteContext context)
         {
             _context = context;
         }
-        //private static List<Pedido> _pedidos = null;
 
 
-        private static List<Pedido> _pedidos = new List<Pedido>();
         [HttpGet]
 
         public async Task<ActionResult<List<Pedido>>> GetPedidos()
@@ -28,35 +28,44 @@ namespace LabAWS_RestauranteAPI.Controllers
         }
 
         // Requerimiento: Un mozo toma el pedido de una...
+        // IActionResult proporciona más flexibilidad en el caso de cambiar el tipo de resultado 
         [HttpPost]
-        public async Task<IActionResult> CreatePedido(int idComanda,int idProducto,int cantidad,int idEstado, string? codigoCliente = null,string? observaciones = null)
+        public async Task<IActionResult> CreatePedido( string descripcion, int cantidad, int idEstado,int demoraEstipulada, string? codigoCliente = null, string? observaciones = null)
         {
             if (cantidad <= 0)
             {
                 return BadRequest("La cantidad es obligatoria y debe ser mayor a cero.");
             }
 
-            // aca lo esta creando
+            // aca lo esta creando pero falta la descripcion porque pedido no tiene descripcon pero si producto
+            //falta que a la hora de tomar el pedido lo relacione con la comanda y id de producto
+            //falta que pedido tenga una lista de productos
             var pedido = new Pedido
-            {
-                IdPedido = _pedidos.Any() ? _pedidos.Max(x => x.IdPedido) + 1 : 1,
-                IdComanda = idComanda,
-                IdProducto = idProducto,
+            {   
                 Cantidad = cantidad,
                 IdEstado = idEstado,
                 FechaCreacion = DateTime.Now,
                 CodigoCliente = codigoCliente,
                 Observaciones = observaciones
+
             };
+            try
+            {
+                _context.Pedidos.Add(pedido);
+                await _context.SaveChangesAsync();
 
-           
-            _pedidos.Add(pedido);//aca lo agrega
-            //_context.Pedidos.Add(pedido);
-            await _context.SaveChangesAsync();// se lo agregue recien
+                return CreatedAtAction(nameof(GetPedidoById), new { id = pedido.IdPedido }, pedido);
 
-            return CreatedAtAction(nameof(GetPedidoById), new { id = pedido.IdPedido }, pedido);
+            }
+
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error al guardar el pedido: {ex.Message}");
+            }
+
         }
 
+        // este hay que reemplazarlo por codigo cliente?
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPedidoById(int id)
